c4ce74b03d66184004847f18900f072e1ea5c0a3
valed-dm <dmvaled@gmail.com>
2021-08-15 11:03:26 +0300

    Delete Many Documents Passed
    with model.remove()

--- myApp.js
+++ myApp.js
@@ -82,8 +82,9 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = done => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, data) =>
+    err ? done(err) : done(null, data)
+  );
 };
 
 const queryChain = done => {
